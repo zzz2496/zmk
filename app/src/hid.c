5f02f3caa562691a5fac5ee268a4d514337ef9ca
Alexander Krikun <krikun98@gmail.com>
2021-04-28 15:41:03 +0300

    button press/release fix

--- app/src/hid.c
+++ app/src/hid.c
@@ -137,7 +137,7 @@ void zmk_hid_consumer_clear() { memset(&consumer_report.body, 0, sizeof(consumer
 
 // Keep track of how often a button was pressed.
 // Only release the button if the count is 0.
-static int explicit_button_counts[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+static int explicit_button_counts[3] = {0, 0, 0};
 static zmk_mod_flags_t explicit_buttons = 0;
 
 #define SET_MOUSE_BUTTONS(butts)                                                               \
@@ -148,21 +148,21 @@ static zmk_mod_flags_t explicit_buttons = 0;
     }
 
 int zmk_hid_mouse_button_press(zmk_mouse_button_t button) {
-    explicit_button_counts[button]++;
-    LOG_DBG("Button %d count %d", button, explicit_button_counts[button]);
+    explicit_button_counts[button-5]++;
+    LOG_DBG("Button %d count %d", button, explicit_button_counts[button-5]);
     WRITE_BIT(explicit_buttons, button, true);
     SET_MOUSE_BUTTONS(explicit_buttons);
     return 0;
 }
 
 int zmk_hid_mouse_button_release(zmk_mouse_button_t button) {
-    if (explicit_button_counts[button] <= 0) {
+    if (explicit_button_counts[button-5] <= 0) {
         LOG_ERR("Tried to release button %d too often", button);
         return -EINVAL;
     }
-    explicit_button_counts[button]--;
-    LOG_DBG("Button %d count: %d", button, explicit_button_counts[button]);
-    if (explicit_button_counts[button] == 0) {
+    explicit_button_counts[button-5]--;
+    LOG_DBG("Button %d count: %d", button, explicit_button_counts[button-5]);
+    if (explicit_button_counts[button-5] == 0) {
         LOG_DBG("Button %d released", button);
         WRITE_BIT(explicit_buttons, button, false);
     }
@@ -171,7 +171,7 @@ int zmk_hid_mouse_button_release(zmk_mouse_button_t button) {
 }
 
 int zmk_hid_mouse_buttons_press(zmk_mouse_button_flags_t buttons) {
-    for (zmk_mod_t i = 0; i < 8; i++) {
+    for (zmk_mod_t i = 5; i < 8; i++) {
         if (buttons & (1 << i)) {
             zmk_hid_mouse_button_press(i);
         }
@@ -180,7 +180,7 @@ int zmk_hid_mouse_buttons_press(zmk_mouse_button_flags_t buttons) {
 }
 
 int zmk_hid_mouse_buttons_release(zmk_mouse_button_flags_t buttons) {
-    for (zmk_mod_t i = 0; i < 8; i++) {
+    for (zmk_mod_t i = 5; i < 8; i++) {
         if (buttons & (1 << i)) {
             zmk_hid_mouse_button_release(i);
         }
