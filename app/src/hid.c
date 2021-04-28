4b270f8f460e475b4e6beafcba1f668cb60af57c
Alexander Krikun <krikun98@gmail.com>
2021-04-28 17:36:13 +0300

    new report sent

--- app/src/hid.c
+++ app/src/hid.c
@@ -16,7 +16,7 @@ static struct zmk_hid_keyboard_report keyboard_report = {
 static struct zmk_hid_consumer_report consumer_report = {.report_id = 2, .body = {.keys = {0}}};
 
 static struct zmk_hid_mouse_report mouse_report = {.report_id = 3, .body = {
-    .buffer = 1, .buttons = 0, .x = 0, .y = 0}};
+    .buttons = 0, .x = 0, .y = 0}};
 
 // Keep track of how often a modifier was pressed.
 // Only release the modifier if the count is 0.
@@ -137,7 +137,7 @@ void zmk_hid_consumer_clear() { memset(&consumer_report.body, 0, sizeof(consumer
 
 // Keep track of how often a button was pressed.
 // Only release the button if the count is 0.
-static int explicit_button_counts[3] = {0, 0, 0};
+static int explicit_button_counts[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 static zmk_mod_flags_t explicit_buttons = 0;
 
 #define SET_MOUSE_BUTTONS(butts)                                                               \
@@ -171,7 +171,7 @@ int zmk_hid_mouse_button_release(zmk_mouse_button_t button) {
 }
 
 int zmk_hid_mouse_buttons_press(zmk_mouse_button_flags_t buttons) {
-    for (zmk_mod_t i = 5; i < 8; i++) {
+    for (zmk_mod_t i = 0; i < 16; i++) {
         if (buttons & (1 << i)) {
             zmk_hid_mouse_button_press(i);
         }
@@ -180,7 +180,7 @@ int zmk_hid_mouse_buttons_press(zmk_mouse_button_flags_t buttons) {
 }
 
 int zmk_hid_mouse_buttons_release(zmk_mouse_button_flags_t buttons) {
-    for (zmk_mod_t i = 5; i < 8; i++) {
+    for (zmk_mod_t i = 0; i < 16; i++) {
         if (buttons & (1 << i)) {
             zmk_hid_mouse_button_release(i);
         }
