a58449d2cbf89a0747605fc525afb4ae5b20482a
Dmitry Tsykunov <>
2021-04-29 18:11:28 +0300

    copy paste fix

--- app/include/zmk/events/mouse_state_changed.h
+++ app/include/zmk/events/mouse_state_changed.h
@@ -23,8 +23,8 @@ static inline struct zmk_mouse_state_changed_event *
 zmk_mouse_state_changed_from_encoded(uint32_t encoded, bool pressed,
                                      int64_t timestamp) {
 
-  uint32_t x = (binding->param1 & 0xFFFF0000) >> 16;
-  uint32_t y = binding->param1 & 0x0000FFFF;
+  uint32_t x = (encoded & 0xFFFF0000) >> 16;
+  uint32_t y = encoded & 0x0000FFFF;
   LOG_DBG("x: 0x%02X y: 0x%02X", x, y);
   zmk_hid_mouse_movement_release(x, y);
 
