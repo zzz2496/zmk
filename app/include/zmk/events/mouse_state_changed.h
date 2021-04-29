c235dfb3aa0d4f46a1382d386d849e7fd28f72c9
Dmitry Tsykunov <>
2021-04-29 18:18:11 +0300

    remove extra release

--- app/include/zmk/events/mouse_state_changed.h
+++ app/include/zmk/events/mouse_state_changed.h
@@ -10,7 +10,6 @@
 #include <zephyr.h>
 #include <zmk/event_manager.h>
 
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 struct zmk_mouse_state_changed {
   uint32_t x;
@@ -27,8 +26,6 @@ zmk_mouse_state_changed_from_encoded(uint32_t encoded, bool pressed,
 
   uint32_t x = (encoded & 0xFFFF0000) >> 16;
   uint32_t y = encoded & 0x0000FFFF;
-  LOG_DBG("x: 0x%02X y: 0x%02X", x, y);
-  zmk_hid_mouse_movement_release(x, y);
 
   return new_zmk_mouse_state_changed((struct zmk_mouse_state_changed){
       .x = x, .y = y, .state = pressed, .timestamp = timestamp});
