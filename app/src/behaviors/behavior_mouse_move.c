64c0b2f68102f4f8263c03cedee69b8d8b0cbc30
Dmitry Tsykunov <>
2021-04-30 01:44:15 +0300

    recursive timer

--- app/src/behaviors/behavior_mouse_move.c
+++ app/src/behaviors/behavior_mouse_move.c
@@ -22,24 +22,27 @@ static int behavior_mouse_move_init(const struct device *dev) { return 0; };
 
 static bool mouse_is_moving = false;
 
+void mouse_timer_cb(struct k_timer *dummy);
+
+K_TIMER_DEFINE(mouse_timer, mouse_timer_cb, NULL);
+
 void mouse_timer_cb(struct k_timer *dummy)
 {
     if (mouse_is_moving) {
         // considering that mouse report structure hasn't changed
         zmk_endpoints_send_mouse_report();
+        k_timer_start(&mouse_timer, K_MSEC(10), K_NO_WAIT);
     }
 }
-
-
-K_TIMER_DEFINE(mouse_timer, mouse_timer_cb, NULL);
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    int res = ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, true,
+                                                                event.timestamp));
     mouse_is_moving = true;
     k_timer_start(&mouse_timer, K_MSEC(10), K_NO_WAIT);
-    return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, true,
-                                                                event.timestamp));
+    return res;
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
@@ -48,7 +51,7 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 
     // race condition?
     mouse_is_moving = false;
-    k_timer_stop(&mouse_timer);
+    /* k_timer_stop(&mouse_timer); */
     return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, false,
                                                                 event.timestamp));
 }
