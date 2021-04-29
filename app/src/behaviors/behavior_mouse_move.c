8c72ad6d55ca0c38dcb618549f231b050fb956fc
Dmitry Tsykunov <>
2021-04-29 18:35:05 +0300

    close parens

--- app/src/behaviors/behavior_mouse_move.c
+++ app/src/behaviors/behavior_mouse_move.c
@@ -23,21 +23,21 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
     return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, true,
-                                                                event.timestamp);
+                                                                event.timestamp));
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
                                       struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
     return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, false,
-                                                                event.timestamp);
+                                                                event.timestamp));
 }
 
 static const struct behavior_driver_api behavior_mouse_move_driver_api = {
     .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
 
 #define KP_INST(n)                                                                                 \
-    DEVICE_AND_API_INIT(behavior_mouse_move_##n, DT_INST_LABEL(n), behavior_mouse_move_init, NULL,   \
+    DEVICE_AND_API_INIT(behavior_mouse_move_##n, DT_INST_LABEL(n), behavior_mouse_move_init, NULL, \
                         NULL, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                    \
                         &behavior_mouse_move_driver_api);
 
