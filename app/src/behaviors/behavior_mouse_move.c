32a00b4530ec63b2d8fe18ee252712632ff6567d
Dmitry Tsykunov <>
2021-04-30 01:22:59 +0300

    add mouse move timer

--- app/src/behaviors/behavior_mouse_move.c
+++ app/src/behaviors/behavior_mouse_move.c
@@ -19,9 +19,24 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int behavior_mouse_move_init(const struct device *dev) { return 0; };
 
+static bool mouse_is_moving = false;
+
+void mouse_timer_cb(struct k_timer *dummy)
+{
+    if (mouse_is_moving) {
+        // considering that mouse report structure hasn't changed
+        zmk_endpoints_send_mouse_report();
+    }
+}
+
+
+K_TIMER_DEFINE(mouse_timer, mouse_timer_cb, NULL);
+
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    mouse_is_moving = true;
+    k_timer_start(&mouse_timer, K_MSEC(10), K_NO_WAIT);
     return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, true,
                                                                 event.timestamp));
 }
@@ -29,6 +44,10 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
                                       struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+
+    // race condition?
+    mouse_is_moving = false;
+    k_timer_stop(&mouse_timer);
     return ZMK_EVENT_RAISE(zmk_mouse_state_changed_from_encoded(binding->param1, false,
                                                                 event.timestamp));
 }
