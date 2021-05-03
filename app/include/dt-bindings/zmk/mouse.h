e6f2b3a32dc1ae7fc5c74321cb92fa7ab542fa56
krikun98 <krikun98@gmail.com>
2021-05-03 09:54:35 +0300

    new definitions

--- app/include/dt-bindings/zmk/mouse.h
+++ app/include/dt-bindings/zmk/mouse.h
@@ -28,15 +28,37 @@
 
 #define MB8 (0x80)
 
+#define MB9 (0x0100)
+
+#define MB10 (0x0200)
+
+#define MB11 (0x0400)
+
+#define MB12 (0x0800)
+
+#define MB13 (0x1000)
+
+#define MB14 (0x2000)
+
+#define MB15 (0x4000)
+
+#define MB16 (0x8000)
+
 /* Mouse move behavior */
 
 #define MOVE_UP (0x0000FFFF)
 
 #define MOVE_DOWN (0x00000001)
 
+#define MOVE_VERT(vert) ((vert * (INT16_MAX / 100)))
+
 #define MOVE_LEFT (0xFFFF0000)
 
 #define MOVE_RIGHT (0x00010000)
+
+#define MOVE_HOR(hor) ((hor * (INT16_MAX / 100)) << 16)
+
+#define MOVE(hor, vert) (((hor * (INT16_MAX / 100)) << 16) + ((vert * (INT16_MAX / 100))))
 
 /* Mouse wheel behavior */
 
